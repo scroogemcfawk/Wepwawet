707b0880368f086baeccc627385a707b556646d5
scroogemcfawk <scroogemcfawk@gmail.com>
2022-10-22 20:39:54 +0700

    Update: Code cleanup v0.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,15 +2,17 @@ import scroogemcfawk.Logging;
 
 import java.io.IOException;
 
-/** The {@code Wepwawet} class is used to automatically update stock prices of my mom's
- *  MS Excel table, because seeing manual updating is quite depressive.
+/**
+ * The {@code Wepwawet} class is used to automatically update stock prices of my mom's
+ * MS Excel table, because seeing manual updating is quite depressive.
  *
  * @author scroogemcfawk
- * @version v0.1.1
+ * @version v0.1.3
  */
 public class App
 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
         String s = Logging.ANSI_RED;
     }
 }
--- src/main/java/XCellFetcher.java
+++ src/main/java/XCellFetcher.java
@@ -27,15 +27,18 @@ public class XCellFetcher
     private static String inputFileAbsolutePath;
     private static boolean isInit = false;
 
-    static {
+    static
+    {
         System.out.println("XCellFetcher is loaded");
     }
 
-    public static void pr() {
+    public static void pr()
+    {
         System.out.println("Start row: " + startCell.replaceAll("[a-zA-Z]+", ""));
         System.out.println("Start col: " + startCell.replaceAll("\\d+", ""));
         System.out.println("End row: " + endCell.replaceAll("[a-zA-Z]+", ""));
         System.out.println("End col: " + endCell.replaceAll("\\d+", ""));
+        System.out.println("Filepath: " + inputFileAbsolutePath);
     }
 
     public static void initFetcher() throws IOException
@@ -51,9 +54,12 @@ public class XCellFetcher
         System.out.println("Absolute path to input file: (e.g " + System.getProperty("user.dir") + "): ");
         String path = in.nextLine().strip();
 
-        try {
+        try
+        {
             initFetcher(start, end, path);
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             System.out.println(Logging.red("Failed to init the fetcher"));
             var rethrow = new IOException("initFetcher(String, String, String) failed");
             rethrow.initCause(e);
@@ -61,20 +67,25 @@ public class XCellFetcher
         }
     }
 
-    public static void initFetcher(String startCell, String endCell, String inputFileAbsolutePath)
-    throws IOException
+    public static void initFetcher(String startCell, String endCell, String inputFileAbsolutePath) throws IOException
     {
-        if (startCell.matches("[a-zA-Z]+\\d+") && endCell.matches("[a-zA-Z]+\\d+")) {
+        if (startCell.matches("[a-zA-Z]+\\d+") && endCell.matches("[a-zA-Z]+\\d+") && startCell.replaceAll("\\d", "").equals(endCell.replaceAll("\\d", "")))
+        {
             XCellFetcher.startCell = startCell;
             XCellFetcher.endCell = endCell;
-        } else {
+        }
+        else
+        {
             throw new IOException("Bad input format given while initializing XCellFetcher");
         }
-        try {
+        try
+        {
             new FileInputStream(inputFileAbsolutePath).close();
             XCellFetcher.inputFileAbsolutePath = inputFileAbsolutePath;
             isInit = true;
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e)
+        {
             throw new IOException("Could not find input file");
         }
     }
@@ -82,26 +93,32 @@ public class XCellFetcher
 
     public static @NotNull ArrayList<Asset> fetch(String file) throws FetchingException
     {
-        try (FileInputStream in = new FileInputStream(file)) {
-            if (!isInit) {
+        try (FileInputStream in = new FileInputStream(file))
+        {
+            if (!isInit)
+            {
                 initFetcher();
             }
             ArrayList<Asset> res = new ArrayList<>(20);
             Workbook wb = new XSSFWorkbook(in);
             Sheet sh = wb.getSheetAt(0);
-            for (int i = 7; i < 45; i++) {
+            for (int i = 7; i < 45; i++)
+            {
                 Row r = sh.getRow(i);
                 Cell c = r.getCell(2);
                 //                System.out.printf(c.toString());
                 String t = c.toString();
                 double p = r.getCell(5).getNumericCellValue();
                 //                System.out.printf("" + p);
-                if (!t.isBlank()) {
+                if (!t.isBlank())
+                {
                     res.add(new Asset(t, p));
                 }
             }
             return res;
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             FetchingException rethrow = new FetchingException("File exception in fetcher");
             rethrow.initCause(e);
             throw rethrow;
@@ -110,13 +127,12 @@ public class XCellFetcher
 
     public static void main(String[] args) throws IOException
     {
-//        Logger.getGlobal().setLevel(Level.OFF);
-//        ArrayList<Asset> assets = fetch("./io/FinanceWorkbook.xlsx");
-//        for (Asset asset: assets) {
-//            System.out.println(asset + " -> " + Logging.red("" + Parser.parse(asset)));
-//        }
-        initFetcher("A51", "B1",
-                "C:\\Users\\scroo\\IdeaProjects\\Wepwawet\\io\\FinanceWorkbook.xlsx");
+        //        Logger.getGlobal().setLevel(Level.OFF);
+        //        ArrayList<Asset> assets = fetch("./io/FinanceWorkbook.xlsx");
+        //        for (Asset asset: assets) {
+        //            System.out.println(asset + " -> " + Logging.red("" + Parser.parse(asset)));
+        //        }
+        initFetcher();
         XCellFetcher.pr();
     }
 }
