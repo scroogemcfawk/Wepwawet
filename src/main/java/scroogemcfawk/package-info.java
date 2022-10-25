eb12e3daa85b198f74739ded92386f5de189d175
scroogemcfawk <scroogemcfawk@gmail.com>
2022-10-25 17:59:09 +0700

    Update: fuck me v0.3.1

---
+++ src/main/java/scroogemcfawk/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * What are you doing in my swamp?!
+ * <br><br>
+ * {@link scroogemcfawk.Color} - colorful output using ANSI escape codes
+ * @author scroogemcfawk
+ */
+package scroogemcfawk;
